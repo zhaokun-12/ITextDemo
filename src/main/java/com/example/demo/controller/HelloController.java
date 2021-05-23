6ca6c336b9e7c575ac16380369a23e1c1661608b
z1158854067 <1158854067@qq.com>
2021-05-23 21:26:32 +0800

    xmlParse可以实现无边距， html转换暂时不可以 关键点在于itextRendereMargin 类中的paintPage 方法

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.test.HTMLToPDF;
+import com.example.demo.utils.HtmlToPdfUtils;
+import com.example.demo.utils.ITextRendererMargin;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.w3c.dom.Document;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
@@ -22,9 +27,12 @@ import java.util.HashMap;
  **/
 @Controller
 public class HelloController {
-
+    private static String path = "C:\\Users\\45585\\Desktop/";
     @Autowired
     private TemplateEngine templateEngine;
+
+    @Autowired
+    private HtmlToPdfUtils htmlToPdfUtils;
     @GetMapping("hello")
     public String jump() throws Exception {
         Context context = new Context();
@@ -32,7 +40,8 @@ public class HelloController {
         String process = templateEngine.process("认申购确认书-模板一", context);
         System.out.println(process);
 
-        ITextRenderer renderer = new ITextRenderer();
+//        ITextRenderer renderer = new ITextRendererMargin();
+        ITextRendererMargin renderer = new ITextRendererMargin();
         ITextFontResolver fontResolver = renderer.getFontResolver();
         //设置字体，否则不支持中文,在html中使用字体，html{ font-family: SimSun;}
         fontResolver.addFont("static/font/simsun.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
@@ -40,7 +49,7 @@ public class HelloController {
         renderer.layout();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         renderer.createPDF(outputStream);
-        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\meix\\Desktop/9900.pdf");
+        FileOutputStream fileOutputStream = new FileOutputStream(path + "9901.pdf");
         fileOutputStream.write(outputStream.toByteArray());
         fileOutputStream.close();
         outputStream.close();
@@ -54,11 +63,20 @@ public class HelloController {
         String process = templateEngine.process("认申购确认书-模板一", context);
         System.out.println(process);
         ByteArrayOutputStream byteArrayOutputStream = new HTMLToPDF().htmlToPdf(process);
-        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\meix\\Desktop/9900.pdf");
+        FileOutputStream fileOutputStream = new FileOutputStream(path + "9900.pdf");
         fileOutputStream.write(byteArrayOutputStream.toByteArray());
         fileOutputStream.close();
         return "认申购确认书-模板一";
     }
+
+    @GetMapping("hello4")
+    public String jump4() throws Exception {
+        String outputFile = path + "9999.pdf";
+        htmlToPdfUtils.htmlToPdfFindalPlus("认申购确认书-模板一", null, outputFile, PageSize.A4, "", true, null);
+        return "认申购确认书-模板一";
+    }
+
+
 
 //    @GetMapping("hello3")
 //    //itext7
--- src/main/java/com/example/demo/utils/HtmlToPdfUtils.java
+++ src/main/java/com/example/demo/utils/HtmlToPdfUtils.java
@@ -1,17 +1,37 @@
 package com.example.demo.utils;
 
-import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfEncodings;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.Pipeline;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.html.CssAppliersImpl;
+import com.itextpdf.tool.xml.html.Tags;
+import com.itextpdf.tool.xml.net.FileRetrieve;
+import com.itextpdf.tool.xml.net.ReadingProcessor;
+import com.itextpdf.tool.xml.parser.XMLParser;
+import com.itextpdf.tool.xml.pipeline.css.CSSResolver;
+import com.itextpdf.tool.xml.pipeline.css.CssResolverPipeline;
+import com.itextpdf.tool.xml.pipeline.end.PdfWriterPipeline;
+import com.itextpdf.tool.xml.pipeline.html.AbstractImageProvider;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipeline;
+import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
+import com.itextpdf.tool.xml.pipeline.html.ImageProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextFontResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +44,9 @@ import java.util.Map;
 @Service
 @Slf4j
 public class HtmlToPdfUtils {
+
+    final String charsetName = "UTF-8";
+
     @Autowired
     TemplateEngine templateEngine;
 
@@ -42,6 +65,132 @@ public class HtmlToPdfUtils {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         renderer.createPDF(outputStream);
         return outputStream;
+
+    }
+
+    public  void htmlToPdfFindalPlus(String htmlTemplate, Map<String, Object> dataMap,
+String targetPdf, Rectangle pageSize, String header, boolean isFooter, File watermark) throws IOException, DocumentException {
+        Context context = new Context();
+        context.setVariables(new HashMap<>());
+        // 获取html文本
+        String process = templateEngine.process(htmlTemplate, context);
+
+        Document document = new Document(pageSize);
+        OutputStream outputStream = new FileOutputStream(targetPdf);
+        PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+        // 设置页边距
+        document.setMargins(0,0,0,0);
+        PDFBuilder builder = new PDFBuilder("页眉", 10, pageSize, null, true);
+        // 事件回调
+        writer.setPageEvent(builder);
+        document.open();
+        HtmlPipelineContext htmlContext = new HtmlPipelineContext(new CssAppliersImpl(new XMLWorkerFontProvider(){
+//            @Override
+//            public Font getFont(String fontname, String encoding, float size, final int style) {
+//                if (fontname == null) {
+///**
+//                * 操作系统需要有该字体, 没有则需要安装; 当然也可以将字体放到项目中， 再从项目中读取
+//                */
+//                    fontname = "simsun.ttf";
+//                    encoding = "UniGB-UCS2-H";
+//                }
+//                Font font = null;
+//                try {
+//                    font = new Font(BaseFont.createFont(fontname, encoding, BaseFont.NOT_EMBEDDED), size, style);
+//                } catch (Exception e) {
+//                    log.error("", e);
+//                }
+//                return font;
+//            }
+        })){
+            @Override
+                public HtmlPipelineContext clone() throws CloneNotSupportedException {
+                    HtmlPipelineContext context = super.clone();
+                    ImageProvider imageProvider = this.getImageProvider();
+                    context.setImageProvider(imageProvider);
+                    return context;
+            }
+        };
+
+        /**
+              * 图片解析
+              */
+//        htmlContext.setImageProvider(new AbstractImageProvider() {
+//            String rootPath = "C:\\Users\\Administrator\\Desktop\\刘亦菲\\";
+//
+//            @Override
+//            public String getImageRootPath() {
+//                return rootPath;
+//            }
+//
+//            @Override
+//            public Image retrieve(String src) {
+//                if (StringUtils.isEmpty(src)) {
+//                    return null;
+//                }
+//                try { Image image = Image.getInstance(new File(rootPath, src).toURI().toString());
+//                    /**
+//                                * 图片显示位置
+//                                */
+//                    image.setAbsolutePosition(400, 400);
+//                    if (image != null) {
+//                        store(src, image);
+//                        return image;
+//                    }
+//                } catch (Exception e) {
+//                    log.error("", e);
+//                }
+//                return super.retrieve(src);
+//            }
+//        });
+        htmlContext.setAcceptUnknown(true).autoBookmark(true)
+                .setTagFactory(Tags.getHtmlTagProcessorFactory());
+
+        /**
+              * css解析
+              */
+        CSSResolver cssResolver = XMLWorkerHelper.getInstance().getDefaultCssResolver(true);
+        cssResolver.setFileRetrieve(new FileRetrieve() {
+            @Override
+            public void processFromStream(InputStream in, ReadingProcessor processor) throws IOException {
+                try (InputStreamReader reader = new InputStreamReader(in, charsetName)) {
+                    int i = -1;
+                    while (-1 != (i = reader.read())) {
+                        processor.process(i);
+                    }
+                } catch (Throwable e) {
+                }
+            }
+
+            /**
+                  * 解析href
+                  */
+            @Override
+            public void processFromHref(String href, ReadingProcessor processor) throws IOException {
+                InputStream is = new ByteArrayInputStream(href.getBytes());
+                try {
+                    InputStreamReader reader = new InputStreamReader(is, charsetName);
+                    int i = -1;
+                    while (-1 != (i = reader.read())) {
+                        processor.process(i);
+                    }
+                } catch (Exception e) {
+                    log.error("", e);
+                }
+
+            }
+        });
+
+        HtmlPipeline htmlPipeline =
+                new HtmlPipeline(htmlContext, new PdfWriterPipeline(document, writer));
+        Pipeline<?> pipeline = new CssResolverPipeline(cssResolver, htmlPipeline);
+        XMLWorker worker = null;
+        worker = new XMLWorker(pipeline, true);
+        XMLParser parser = new XMLParser(true, worker, Charset.forName(charsetName));
+        try (InputStream inputStream = new ByteArrayInputStream(process.getBytes())) {
+            parser.parse(inputStream, Charset.forName(charsetName));
+        }
+        document.close();
 
     }
 }
